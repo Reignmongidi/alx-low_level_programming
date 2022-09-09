3633211b0d974029aa2ebcb3aada5a02408a6f00
Reignmongidi <mojeedafolarin2@gmail.com>
2022-09-08 20:09:49 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* into the first C */
 
 int main (void)  {
-	puts ("\"Programming is like building a multilingual puzzle"\n);
+	puts ("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 
 	}
