dcde970a30c3a7e6bdc8f5ee8efb197c8750a097
Reignmongidi <mojeedafolarin2@gmail.com>
2022-09-08 21:17:21 -0700

    okay

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * into the first C
- * main -prints a space
- * Return: 0
-  */
-
-int main(void) 
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle`\n");
-	return (0);
+puts ("\"Programming is like building a multilingual puzzle");
+return(0); 
 }
