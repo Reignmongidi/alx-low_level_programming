4dd87b30480dc8eb32c1ae89de565ca438b0704f
Reignmongidi <mojeedafolarin2@gmail.com>
2022-09-14 15:11:42 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
-/* File- 0-putchar.c
+/*
+ * File- 0-putchar.c
  * Auth- Mojeed Azeez
+ * main - prints stdout
+ * returns : always 0
 */
 
 #include "main.h"
 /**
- * main - prints "_putchar" followed by a new line.
- * return: Always 0 (success.)
+ * Main.h - prints 'putcharstring[i]' followed by a new line.
+ * Return: Always 0 (success).
+ * main.h - newly created header function
 */
 int main(void)
 
 {
-	char putcharstring[8] = "_putchar";
-	int i;
+char putcharstring[8] = "_putchar"
+for (i = 0; i < 8; i++)
+_putchar(putcharstring[i]);
+_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-		_putchar(putcharstring[i]);
-	_putchar('\n');
-
-	return (0);
+return (0);
 }
