11acc3a5b9c38b05dfd4098f9265b0898b0592ee
Reignmongidi <mojeedafolarin2@gmail.com>
2022-09-22 19:02:05 -0700

    do already

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,17 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
 
-	_strcat(dest, src);
-return (0);
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+
+	return (dest);
 }
