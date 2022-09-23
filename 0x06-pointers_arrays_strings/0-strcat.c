5ae8a54fbb19b77eb8ca36a7a303c3570cb90379
Reignmongidi <mojeedafolarin2@gmail.com>
2022-09-22 18:14:53 -0700

    done yet

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcat - Concatenates two strings
+ *
+ * Main - Check the code
+ * Return: Always zero
+ *
+ **/
+
+char *_strcat(char *dest, char *src)
+{
+
+	strcat(dest, src);
+return (0);
+}
